9d0e7d730f7ecc28f35690b16de8e45076e0c96f
Pann Cherry <panncherry@Panns-MacBook-Pro.local>
2018-09-10 21:22:14 -0400

    Updated

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Pann Cherry on 9/4/18.
+//  Copyright © 2018 Pann Cherry. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
